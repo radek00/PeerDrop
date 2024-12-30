1999cddb23248e02880e17ed2a018c2d46112d4d
Radekor500 <radek00500@gmail.com>
2024-12-30 13:30:01 +0100

    very basic hub demo

---
+++ LocalShare/Hubs/WebRtcSignalingHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LocalShare.Hubs
+{
+    public class WebRtcSignalingHub: Hub
+    {
+        public static readonly string Url = "/signalling";
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
--- LocalShare/Program.cs
+++ LocalShare/Program.cs
@@ -1,12 +1,31 @@
+using LocalShare.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 
+app.MapGet("/", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("ClientApp/index.html");
+});
 // Configure the HTTP request pipeline.
+
+app.MapHub<WebRtcSignalingHub>(WebRtcSignalingHub.Url);
 
 app.UseAuthorization();
 
