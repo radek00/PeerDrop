01a348bb8a8e78814fb41beedb359e711ec07872
Radekor500 <radek00500@gmail.com>
2025-01-17 15:12:26 +0100

    serviceWorker fake download WIP

--- LocalShare/ClientApp/serviceWorker.js
+++ LocalShare/ClientApp/serviceWorker.js
@@ -1,5 +1,4 @@
-﻿const broadcast = new BroadcastChannel('worker');
-self.addEventListener("install", () => {
+﻿self.addEventListener("install", () => {
     self.skipWaiting();
 });
 
@@ -7,17 +6,19 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
 });
 
+const chunkStream = new ChunkStream();
+
 self.addEventListener("fetch", (event) => {
     if (event.request.method !== "GET") return;
     if (new URL(event.request.url).pathname === '/download') {
         const headers = {
-            'Content-Type': 'application/octet-stream',
-            'Content-Disposition': 'attachment; filename="file.txt"',
-            'Content-Length': 100
+            'Content-Type': 'image/jpg',
+            'Content-Disposition': 'attachment; filename="index.jpg"',
         }
         console.log('download');
+        const stream = new ReadableStream(chunkStream);
         event.respondWith(
-            new Response('Hello World!', { headers })
+            new Response(stream, { headers })
         );
         //     event.respondWith(
         //         decrypt(
@@ -27,3 +28,37 @@ self.addEventListener("fetch", (event) => {
         // );
     }
 });
+
+class ChunkStream {
+    fileMetadata;
+    receivedSize = 0;
+    broadcast = new BroadcastChannel('worker');
+    
+    constructor(fileMetadata) {
+        this.fileMetadata = fileMetadata;
+    }
+    
+    start(controller) {
+        this.broadcast.onmessage = (event) => {
+            controller.enqueue(new Uint8Array(event.data.chunkData));
+            if (this.fileMetadata == null) {
+                this.fileMetadata = event.data.fileTransferMetadata;
+            }
+            this.receivedSize += event.data.chunkData.byteLength;
+
+            if (this.receivedSize >= this.fileMetadata.size) {
+                controller.close();
+                this.broadcast.close();
+            }
+        }
+    }
+    
+    pull(controller) {
+        // Do nothing
+    }
+    
+    cancel(reason) {
+        console.log(reason);
+    }
+    
+}
