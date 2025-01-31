db383fedb786ce461c9fb618fb119a60596498e0
Radekor500 <radek00500@gmail.com>
2025-01-31 14:19:27 +0100

    vite config and registering service worker

---
+++ LocalShare/ClientApp/serviceWorker.ts
@@ -0,0 +1,9 @@
+/// <reference lib="WebWorker" />
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
---
+++ LocalShare/ClientApp/vite.config.ts
@@ -0,0 +1,67 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, UserConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+
+const baseConfig: UserConfig = {
+  plugins: [mkcert()],
+  envDir: "./env",
+  base: "/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    https: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5033",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path: string) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+};
+
+export default defineConfig((config: UserConfig): UserConfig => {
+  if (config.mode === "production") {
+    return {
+      ...baseConfig,
+      build: {
+        emptyOutDir: true,
+        rollupOptions: {
+          input: {
+            app: "./index.html",
+          },
+          output: {
+            entryFileNames: () => "assets/js/[name]-[hash].js",
+            inlineDynamicImports: false,
+          },
+        },
+      },
+    };
+  }
+
+  if (config.mode === "worker") {
+    return {
+      ...baseConfig,
+      build: {
+        emptyOutDir: false,
+        rollupOptions: {
+          input: {
+            serviceWorker: "./serviceWorker.ts",
+          },
+          output: {
+            entryFileNames: () => "[name].js",
+            inlineDynamicImports: true,
+          },
+        },
+      },
+    };
+  }
+  return baseConfig;
+});
