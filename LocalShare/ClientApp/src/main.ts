5a11a2586da6aa54bab6dd23f81198c22155a0d8
Radekor500 <radek00500@gmail.com>
2025-01-31 14:19:27 +0100

    vite config and registering service worker

--- LocalShare/ClientApp/src/main.ts
+++ LocalShare/ClientApp/src/main.ts
@@ -1,22 +1,23 @@
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register(
-                `/serviceWorker.js`,
-                { type: "classic", scope: "/" }
-            );
-
-            if (registration.installing) {
-                console.log("Service worker installing");
-            } else if (registration.waiting) {
-                console.log("Service worker installed");
-            } else if (registration.active) {
-                console.log("Service worker active");
-            }
-
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
+      try {
+        const registration = await navigator.serviceWorker.register(
+          `${import.meta.env.BASE_URL}${import.meta.env.VITE_WORKER}`,
+          { scope: "/" }
+        );
+        const devMode = import.meta.env.DEV;
+        if (devMode) {
+          if (registration.installing) {
+            console.log("Service worker installing");
+          } else if (registration.waiting) {
+            console.log("Service worker installed");
+          } else if (registration.active) {
+            console.log("Service worker active");
+          }
         }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
     }
-};
-registerServiceWorker();
+  };
+  registerServiceWorker();
