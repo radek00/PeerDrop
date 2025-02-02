6cfee4c1887063580ae2b53976b54c1994b4acd5
Radekor500 <radek00500@gmail.com>
2025-02-02 21:59:50 +0100

    remove pulse animation

--- LocalShare/ClientApp/src/utils/ClientGrid.ts
+++ LocalShare/ClientApp/src/utils/ClientGrid.ts
@@ -3,6 +3,11 @@ enum AnimationState {
     PULSATING
 }
 
+enum CircleColor {
+    NoClients = 197,
+    ClientConnected = 52
+}
+
 export class ClientGrid {
     private canvas: HTMLCanvasElement;
     private canvasContext: CanvasRenderingContext2D;
@@ -12,18 +17,14 @@ export class ClientGrid {
     private y0: number;
     private dw: number;
     private step: number = 0;
-    private isAnimating: boolean = true;
-    private pulsateStep: number = 0;
+    private circleColor = CircleColor.NoClients;
 
-    private transitionStep: number = 0;
-    private transitionDuration: number = 120; 
-
-    private circles: { x: number, y: number, radius: number, color: string }[] = [];
-
-    private state: AnimationState = AnimationState.ANIMATING;
+    private strokeMap = new Map<CircleColor, (color: string) => string>();
 
 
     constructor() {
+        this.strokeMap.set(CircleColor.NoClients, (color: string) => `rgba(${color},${color}},${color},0.1)`);
+        this.strokeMap.set(CircleColor.ClientConnected, (color: string) => `rgba(0,${color},0,0.1)`);
         this.canvas = document.createElement('canvas');
         this.canvas.style.width = "100%";
         this.canvas.style.zIndex = "-1";
@@ -48,9 +49,9 @@ export class ClientGrid {
         this.canvas.width = this.width;
         this.canvas.height = this.height;
 
-        let offset = this.height /2;
-        // let offset = this.height > 380 ? 100 : 65;
-        // offset = this.height > 800 ? 116 : offset;
+        //let offset = this.height /2;
+        let offset = this.height > 380 ? 100 : 65;
+        offset = this.height > 800 ? 116 : offset;
         this.x0 = this.width / 2;
         this.y0 = this.height - offset;
         this.dw = Math.max(this.width, this.height, 1000) / 13;
@@ -60,17 +61,19 @@ export class ClientGrid {
     
     private drawCircle(radius: number) {
         this.canvasContext.beginPath();
-        let color = Math.round(197 * (1 - radius / Math.max(this.width, this.height)));
-        this.canvasContext.strokeStyle = 'rgba(' + color + ',' + color + ',' + color + ',0.1)';
+        const intensity = Math.round(197 * (1 - radius / Math.max(this.width, this.height)));
+        
+        if (this.circleColor === CircleColor.NoClients) {
+            this.canvasContext.strokeStyle = `rgba(${intensity},${intensity},${intensity},0.2)`;
+        } else {
+            this.canvasContext.strokeStyle = `rgba(52,${intensity},50,0.3)`;
+    }
         this.canvasContext.arc(this.x0, this.y0, radius, 0, 2 * Math.PI);
         this.canvasContext.stroke();
         this.canvasContext.lineWidth = 2;
-        this.circles.push({ x: this.x0, y: this.y0, radius: radius, color: 'rgba(' + color + ',' + color + ',' + color + ',0.1)' });
     }
 
     private drawCircles() {
-        console.log("Drawing Circles", this.circles);
-        this.circles = [];
         this.canvasContext.clearRect(0, 0, this.width, this.height);
         for (let i = 0; i < 8; i++) {
             this.drawCircle(this.dw * i + this.step % this.dw);
@@ -82,98 +85,23 @@ export class ClientGrid {
         this.init();
         this.animate();
     }
-    private getRandomPointOnCirclePerimeter(circle: { x: number, y: number, radius: number }): { x: number, y: number } {
-        const angle = Math.random() * 2 * Math.PI;
-        const x = circle.x + circle.radius * Math.cos(angle);
-        const y = circle.y + circle.radius * Math.sin(angle);
-        return { x, y };
-    }
-
-    private redrawCircles() {
-        this.canvasContext.clearRect(0, 0, this.width, this.height);
-        for (const circle of this.circles) {
-            this.canvasContext.beginPath();
-            this.canvasContext.strokeStyle = circle.color;
-            this.canvasContext.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
-            this.canvasContext.stroke();
-            this.canvasContext.lineWidth = 4;
-        }
-    }
 
     private animate() {
-        if (this.state === AnimationState.ANIMATING) {
             requestAnimationFrame(() => {
+                console.log(this.circleColor)
                 this.drawCircles();
                 this.animate();
             });
-        }
     }
 
-    
-    private pulsate() {
-        if (this.state === AnimationState.PULSATING) {
-            requestAnimationFrame(() => {
-                this.pulsateStep += 0.025;
-                const alpha = 0.5 + 0.5 * (Math.sin(this.pulsateStep) + 1) / 2; // Value between 0.5 and 1
-                for (let i = 0; i < this.circles.length; i++) {
-                    this.circles[i].color = `rgba(52, 129, 94, ${alpha})`; // Green color with varying alpha
-                }
-                this.redrawCircles();
-                this.pulsate();
-            });
-        }
-    }
 
-    private transition() {
-        console.log(this.circles.length);
-        if (this.transitionStep < this.transitionDuration) {
-            requestAnimationFrame(() => {
-                this.transitionStep++;
-                const progress = this.transitionStep / this.transitionDuration;
-                const alpha = 0.5 + 0.5 * (Math.sin(this.pulsateStep) + 1) / 2; // Value between 0.5 and 1
-                for (let i = 0; i < this.circles.length; i++) {
-                    const currentColor = this.circles[i].color;
-                    const targetColor = `rgba(52, 129, 94, ${alpha})`;
-                    this.circles[i].color = this.interpolateColor(currentColor, targetColor, progress);
-                }
-                this.redrawCircles();
-                this.transition();
-            });
-        } else {
-            this.transitionStep = 0;
-            this.state = AnimationState.PULSATING;
-            this.pulsate();
-        }
-    }
 
-    public getNextClientPosition() {
-        const elementIndex = Math.min(Math.max(0, Math.floor(Math.random() * 3)), this.circles.length - 1);
-        const circle = this.circles[elementIndex];
-        return this.getRandomPointOnCirclePerimeter(circle);
-    }
     public changeStage() {
-        if (this.state === AnimationState.ANIMATING) {
-            this.circles.shift();
-            this.state = AnimationState.PULSATING;
-            this.transition();
+        if (this.circleColor === CircleColor.NoClients) {
+            this.circleColor = CircleColor.ClientConnected;
         } else {
-            this.state = AnimationState.ANIMATING;
-            this.animate();
+            this.circleColor = CircleColor.NoClients;
         }
-    }
-
-    private interpolateColor(currentColor: string, targetColor: string, progress: number): string {
-        const currentRGBA = this.parseRGBA(currentColor);
-        const targetRGBA = this.parseRGBA(targetColor);
-        const interpolatedRGBA = currentRGBA.map((current, index) => {
-            return current + (targetRGBA[index] - current) * progress;
-        });
-        return `rgba(${interpolatedRGBA[0]}, ${interpolatedRGBA[1]}, ${interpolatedRGBA[2]}, ${interpolatedRGBA[3]})`;
-    }
-    
-    private parseRGBA(color: string): number[] {
-        const rgba = color.match(/\d+(\.\d+)?/g)?.map(Number) || [0, 0, 0, 1];
-        return rgba;
     }
     
 }
