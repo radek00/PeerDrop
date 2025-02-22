183387cc0bdf60188727c60a6d3481c75d645941
Radekor500 <radek00500@gmail.com>
2025-02-22 12:22:27 +0100

    render clients with basic incon

--- LocalShare/ClientApp/src/icons/PhoneIcon.ts
+++ LocalShare/ClientApp/src/icons/PhoneIcon.ts
@@ -1,8 +1,17 @@
-export class PhoneIcon extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 25h2M11 3h10a2 2 0 0 1 2 2v22a2 2 0 0 1-2 2H11a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2"/></svg>`
+import { css, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("phone-icon")
+export class PhoneIcon extends LitElement {
+    static styles = css`
+    svg {
+    padding: 12px;
+height: 64px;
+width: 64px;
+border-radius: 50%;
+background-color: #042337;
+  }`;
+    render() {
+        return html`<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 25h2M11 3h10a2 2 0 0 1 2 2v22a2 2 0 0 1-2 2H11a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2"/></svg>`;
     }
 }
-
-customElements.define("phone-icon", PhoneIcon);
