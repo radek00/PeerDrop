f2ecc7f50ae5029e1407d618d5f8fc65fb5148ed
Radekor500 <radek00500@gmail.com>
2025-02-22 12:22:55 +0100

    prettier

--- LocalShare/ClientApp/src/components/ConnectedClient.ts
+++ LocalShare/ClientApp/src/components/ConnectedClient.ts
@@ -5,32 +5,30 @@ import "../icons/PhoneIcon";
 @customElement("connected-client")
 export class ConnectedClient extends LitElement {
   static styles = css`
-  .client {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 3px
-  }
-
-
-  .client--name {
-    color: var(--text-light);
-    font-weight: 600;
-  }
-`;
-    // Make property reactive
-    @property({ type: String })
-    name = "";
-
-
-    render() {
-        console.log('rendering client', this.name);
-        return html`
-            <div class="client">
-                <phone-icon></phone-icon>
-                <div class="client--name">${this.name}</div>
-            </div>
-        `;
+    .client {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 3px;
     }
+
+    .client--name {
+      color: var(--text-light);
+      font-weight: 600;
+    }
+  `;
+  // Make property reactive
+  @property({ type: String })
+  name = "";
+
+  render() {
+    console.log("rendering client", this.name);
+    return html`
+      <div class="client">
+        <phone-icon></phone-icon>
+        <div class="client--name">${this.name}</div>
+      </div>
+    `;
+  }
 }
