1ba7ca84533ed0bfb9fdfd2d87597de07e642d3c
Radekor500 <radek00500@gmail.com>
2025-02-23 12:20:42 +0100

    share icon styles

---
+++ LocalShare/ClientApp/src/styles/sharedStyle.ts
@@ -0,0 +1,10 @@
+import { css } from "lit";
+
+export const clientIconStyles = css`
+    svg {
+      padding: 12px;
+      height: 64px;
+      width: 64px;
+      border-radius: 50%;
+      background-color: var(--color-primary-900);
+    }`
