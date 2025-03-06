6c6fbadfa3ff3d7ff932a887198996d6609282d5
Radekor500 <radek00500@gmail.com>
2025-03-07 00:31:11 +0100

    establish webrtc connection

---
+++ LocalShare/Models/Messages/ReceiveAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalShare.Models.Messages
+{
+    public class ReceiveAnswer
+    {
+        public required string SenderConnectionId { get; set; }
+        public required object Answer { get; set; }
+    }
+}
