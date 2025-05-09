90c01dffd8b4d4e3319ec80f64d304345017aece
Radekor500 <radek00500@gmail.com>
2025-05-09 16:09:02 +0200

    move worker registration to utils

--- LocalShare/ClientApp/src/utils/utils.ts
+++ LocalShare/ClientApp/src/utils/utils.ts
@@ -6,3 +6,29 @@ export function fileSize(size: number) {
     ["B", "kB", "MB", "GB", "TB"][i]
   );
 }
+
+export async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        `${import.meta.env.BASE_URL}${import.meta.env.VITE_WORKER}`,
+        { scope: "/" }
+      );
+      const devMode = import.meta.env.DEV;
+      if (devMode) {
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      }
+      setInterval(() => {
+        navigator.serviceWorker.controller?.postMessage("ping");
+      }, 10000);
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+}
