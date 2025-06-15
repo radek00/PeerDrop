d69d0dfa780f558ee9697bcb65cc12d049c452ab
Radekor500 <radek00500@gmail.com>
2025-06-15 12:07:31 +0200

    improve selectors

--- LocalShare/ClientApp/playwright.config.prod.ts
+++ LocalShare/ClientApp/playwright.config.prod.ts
@@ -3,7 +3,7 @@ import {playwrightConfig} from "./playwright.config";
 
 export default defineConfig({...playwrightConfig,  use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://peerdrop.r-stack.app",
+    baseURL: "https://localhost:5001",
     ignoreHTTPSErrors: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
