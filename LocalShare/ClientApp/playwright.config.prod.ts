8d3ea1a8f97d53374bee16f09e1fb58b3d129aef
Radekor500 <radek00500@gmail.com>
2025-06-15 12:48:16 +0200

    install ssl cert

--- LocalShare/ClientApp/playwright.config.prod.ts
+++ LocalShare/ClientApp/playwright.config.prod.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://localhost:5001",
-    ignoreHTTPSErrors: true,
+    ignoreHTTPSErrors: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
