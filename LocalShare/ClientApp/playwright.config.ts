27281a90e4001440e0133977cf87070d19a2dfd3
Radekor500 <radek00500@gmail.com>
2025-06-15 12:15:29 +0200

    prettier

--- LocalShare/ClientApp/playwright.config.ts
+++ LocalShare/ClientApp/playwright.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="node" />
-import { devices } from '@playwright/test';
+import { devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -11,10 +11,10 @@ import { devices } from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
-*/
+ */
 
-export const playwrightConfig =  {
-  testDir: './tests',
+export const playwrightConfig = {
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,24 +24,24 @@ export const playwrightConfig =  {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -65,16 +65,16 @@ export const playwrightConfig =  {
     // },
   ],
 
-// webServer: {
-//   command: 'pnpm dev',
-//   url: 'https://localhost:3000',
-//   reuseExistingServer: !process.env.CI,
-//   ignoreHTTPSErrors: true,
-//   timeout: 10 * 1000,
-// },
-// use: {
-//   baseURL: 'https://localhost:5001',
-//   ignoreHTTPSErrors: true, // Add this if using self-signed certs
-//   trace: 'on-first-retry',
-// },
+  // webServer: {
+  //   command: 'pnpm dev',
+  //   url: 'https://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  //   ignoreHTTPSErrors: true,
+  //   timeout: 10 * 1000,
+  // },
+  // use: {
+  //   baseURL: 'https://localhost:5001',
+  //   ignoreHTTPSErrors: true, // Add this if using self-signed certs
+  //   trace: 'on-first-retry',
+  // },
 };
