b8273de885001ddfddeba6705cd9397b224fa580
Radekor500 <radek00500@gmail.com>
2025-06-19 14:34:57 +0200

    file upload and download test

---
+++ LocalShare/ClientApp/tests/file-upload-and-download.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from "@playwright/test";
+import { readFile } from "fs";
+
+test.describe("File upload and download", () => {
+
+  test("file is uploaded and received client", async ({ browser }) => {
+    const context1 = await browser.newContext();
+    const context2 = await browser.newContext();
+    const page1 = await context1.newPage();
+    const page2 = await context2.newPage();
+
+    await page1.goto("/");
+    await page2.goto("/");
+
+    const getClientName = async (page) => {
+      const nameElement = page.getByTestId("client-name").first();
+      await expect(nameElement).toBeVisible();
+      const name = await nameElement.innerText();
+      expect(name).not.toBe("");
+      return name;
+    };
+    const client2Name = await getClientName(page2);
+
+    const connectedClient = page1.locator("connected-client", { hasText: client2Name }).first();
+    await expect(connectedClient).toBeVisible();
+
+    const label = connectedClient.locator('xpath=parent::label').first();
+    const fileInput = label.locator('input[type="file"]').first();
+    await fileInput.setInputFiles({
+      name: "testfile.txt",
+      mimeType: "text/plain",
+      buffer: Buffer.from("Hello from client 1!")
+    });
+
+    const confirmDialog = page2.locator('confirm-dialog', {hasText: 'testfile.txt'}).first().locator('.overlay').first();
+    console.log(await confirmDialog.innerHTML());
+    await expect(confirmDialog).toBeVisible({timeout: 15000});
+
+    const button = confirmDialog.locator('button', { hasText: 'Yes' }).first();
+    await expect(button).toBeVisible();
+    const downloadPromise  = page2.waitForEvent('download');
+    await button.click();
+
+    const download = await downloadPromise;
+    const filePath = await download.path();
+    readFile(filePath, 'utf8',(err, data) => {
+        expect(err).toBeNull();
+        expect(data).toBe("Hello from client 1!");
+    })
+    console.log(`File downloaded to: ${filePath}`);
+
+    await page1.close();
+    await context1.close();
+    await page2.close();
+    await context2.close();
+  });
+});
