7ed5d3349d7fb9c94932e1aaa54836f89d327217
Radekor500 <radek00500@gmail.com>
2025-09-01 20:27:32 +0200

    cancellation test

--- LocalShare/ClientApp/tests/file-upload-and-download.spec.ts
+++ LocalShare/ClientApp/tests/file-upload-and-download.spec.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { test, expect, Page, BrowserContext } from "@playwright/test";
 import { readFile } from "fs";
 import { getClientName } from "./utils/utils";
@@ -28,7 +29,8 @@ test.describe.parallel("File upload and download", () => {
     senderPage: Page,
     receiverName: string,
     fileName: string,
-    fileContent: string
+    fileContent: string,
+    size?: number
   ) {
     const connectedClient = senderPage
       .locator("connected-client", { hasText: receiverName })
@@ -36,10 +38,18 @@ test.describe.parallel("File upload and download", () => {
     await expect(connectedClient).toBeVisible();
     const label = connectedClient.locator("xpath=parent::label").first();
     const fileInput = label.locator('input[type="file"]').first();
+    
+    let buffer: Buffer<ArrayBuffer>;
+    if (size) {
+      buffer = Buffer.alloc(size).fill(fileContent);
+    } else {
+      buffer = Buffer.from(fileContent);
+    }
+    
     await fileInput.setInputFiles({
       name: fileName,
       mimeType: "text/plain",
-      buffer: Buffer.from(fileContent),
+      buffer: buffer,
     });
   }
 
@@ -54,10 +64,8 @@ test.describe.parallel("File upload and download", () => {
 
     const confirmDialog = page2
       .locator("confirm-dialog", { hasText: "testfile.txt" })
-      .first()
-      .locator(".overlay")
       .first();
-    await expect(confirmDialog).toBeVisible();
+    
     const button = confirmDialog.locator("button", { hasText: "Yes" }).first();
     await expect(button).toBeVisible();
     const downloadPromise = page2.waitForEvent("download");
@@ -78,6 +86,44 @@ test.describe.parallel("File upload and download", () => {
     await expect(checkmark).toBeVisible();
   });
 
+  test("file upload can be cancelled", async () => {
+    const client2Name = await getClientName(page2);
+    await uploadFileToClient(
+      page1,
+      client2Name,
+      "testfile.txt",
+      "Hello from client 1!",
+      40 * 1024 * 1024 // 40MB
+    );
+
+    const confirmDialog = page2
+      .locator("confirm-dialog", { hasText: "testfile.txt" })
+      .first();
+
+    const button = confirmDialog.locator("button", { hasText: "Yes" }).first();
+    await expect(button).toBeVisible();
+    const downloadPromise = page2.waitForEvent("download");
+    await button.click();
+    (await downloadPromise).cancel();
+
+    const cancellationDialog = page1
+      .locator("confirm-dialog", { hasText: "Transfer cancelled" })
+      .first();
+
+    const confirmationButton = cancellationDialog.locator("button", { hasText: "OK" }).first();
+    await expect(confirmationButton).toBeVisible();
+    await confirmationButton.click();
+
+
+    const client = page1.locator("connected-client", { hasText: client2Name });
+    await expect(client).toBeVisible();
+
+    const waveProgress = client.locator("wave-progress");
+    const error = waveProgress.getByTestId("upload-error");
+    await error.waitFor({ state: "visible", timeout: 15000 });
+    await expect(error).toBeVisible();
+  });
+
   test("File transfer can be rejected by the recipient", async () => {
     const client2Name = await getClientName(page2);
     await uploadFileToClient(
@@ -89,16 +135,14 @@ test.describe.parallel("File upload and download", () => {
 
     const confirmDialog = page2
       .locator("confirm-dialog", { hasText: "testfile.txt" })
-      .first()
-      .locator(".overlay")
       .first();
-    await expect(confirmDialog).toBeVisible();
+
     const button = confirmDialog.locator("button", { hasText: "No" }).first();
     await expect(button).toBeVisible();
     await button.click();
     const rejectedDialogOverlay = page1
-      .locator("confirm-dialog", { hasText: "Transfer rejection" })
-      .locator(".overlay");
+      .locator("confirm-dialog", { hasText: "Transfer rejection" });
+
     await rejectedDialogOverlay.waitFor({ state: "visible", timeout: 15000 });
     await expect(rejectedDialogOverlay).toBeVisible();
   });
