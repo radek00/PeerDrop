c0341c7db0bb272b9cfc49c3ea285fedb24187b6
Radekor500 <radek00500@gmail.com>
2025-09-02 19:26:19 +0200

    change test endpoint

--- LocalShare/ClientApp/playwright.config.prod.ts
+++ LocalShare/ClientApp/playwright.config.prod.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   ...playwrightConfig,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://localhost:5001",
+    baseURL: "http://localhost:5001",
     ignoreHTTPSErrors: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
